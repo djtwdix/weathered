5133ab607129c33b3fc004e257f07d9632c5c6df
djtwdix <djtwd.ix@icloud.com>
2021-03-24 09:23:58 -0300

    Create server index

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const apis = require("./api");
+app.use("/api", apis);
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
